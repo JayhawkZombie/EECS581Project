f5db6abe6f31441cbac7f53a36cf6c12c510a807
Kurt Slagle <kurt_slagle@yahoo.com>
2017-04-08 14:13:58 -0500

    Added asset loading code to BasicLevel class

--- SFEngine/Source/Definitions/Level/BasicLevel.cpp
+++ SFEngine/Source/Definitions/Level/BasicLevel.cpp
@@ -2,6 +2,12 @@
 
 #include "../../Headers/Actor/Player.h"
 #include "../../Headers/Physics/Collider.h"
+#include "../../Headers/Tiles/TileSheet.h"
+
+namespace
+{
+  std::string PROJECT_PATH = "";
+}
 
 namespace Engine
 {
@@ -258,34 +264,177 @@ namespace Engine
 
   void BasicLevel::LoadFromFile(const std::string & file)
   {
+    Json::Value ProjectFile;
+
+    std::ifstream in(file);
+    if (!in) {
+      std::cerr << "Unable to open project file" << std::endl;
+      return;
+    }
+
+    in >> ProjectFile;
+
+    try
+    {
+      PROJECT_PATH = ProjectFile["Project"]["Info"]["PROJECT_PATH"].asString();
+      LoadAssets(ProjectFile["Project"]["Data"]);
+    }
+    catch (EngineRuntimeError &err)
+    {
+      err.AddCause(ExceptionCause::ConstructionError);
+      err.AddMessage(EXCEPTION_MESSAGE("Failed to load assets"));
+      throw;
+    }
   }
 
   void BasicLevel::LoadAssets(const Json::Value & value)
   {
+    LoadTextures(value["Textures"]);
+    LoadAudio(value["Audio"]);
+    LoadTileSheets(value["Sheets"]);
+    LoadAnimations(value["Animations"]);
   }
 
   void BasicLevel::LoadAudio(const Json::Value & value)
   {
+    std::string name{ "" };
+    std::string path{ "" };
+
+    for (auto & file : value) {
+      name = file["Name"].asString();
+      path = PROJECT_PATH + file["Path"].asString();
+
+      SoundBuffers[name] = LoadSoundBuffer(path, name);// std::make_shared<sf::SoundBuffer>();
+      if (!SoundBuffers[name]) {
+        std::cerr << "Failed to load audio file: " << path << std::endl;
+      }
+
+
+    }
   }
 
   void BasicLevel::LoadTextures(const Json::Value & value)
   {
+    std::string texname{ "" };
+    std::string path{ "" };
+
+    for (auto & tex : value) {
+      texname = tex["Name"].asString();
+      path = PROJECT_PATH + tex["Path"].asString();
+      Textures[texname] = LoadTexture(path, texname);// std::make_shared<sf::Texture>();
+      if (!Textures[texname]) {
+        std::cerr << "Unable to load texture: " << path << std::endl;
+      }
+    }
   }
 
   void BasicLevel::LoadTileSheets(const Json::Value & value)
   {
+    for (auto & sheet : value)
+      LoadSheet(sheet);
   }
 
   void BasicLevel::LoadSheet(const Json::Value & value)
   {
+    try
+    {
+      auto texname = value["Name"].asString();
+      auto sheet_data = value["Sheet"];
+      auto frames = sheet_data["Frames"];
+
+      TileSheets[texname] = std::make_shared<TileSheet>();
+
+      Json::Value rect;
+      std::string tilename{ "" };
+      sf::IntRect IRect{ 0, 0, 0, 0 };
+
+      for (auto & frame : frames) {
+        tilename = frame["Name"].asString();
+        rect = frame["Rect"];
+
+        IRect.left = rect[0].asInt();
+        IRect.top = rect[1].asInt();
+        IRect.width = rect[2].asInt();
+        IRect.height = rect[3].asInt();
+
+        TileSheets[texname]->AddTile(texname, IRect);
+        TileSheets[texname]->SetTexture(Textures[sheet_data["Texture"].asString()]);
+      }
+    }
+    catch (Json::Exception &err)
+    {
+      throw EngineRuntimeError({ ExceptionCause::DataParseError }, EXCEPTION_MESSAGE("JSON error"));
+      throw;
+    }
   }
 
   void BasicLevel::LoadAnimations(const Json::Value & value)
   {
+    for (auto & anim : value) {
+
+      std::string name = anim["Name"].asString();
+      std::string texture = anim["Texture"].asString();
+      float time = anim["FrameTime"].asFloat();
+      bool bPingPong = anim["PingPong"].asBool();
+      bool looping = anim["Looping"].asBool();
+
+      auto it = Textures.find(texture);
+      if (it == Textures.end())
+        return;
+
+      auto tex = it->second;
+      Animations[name] = std::make_shared<Engine::Animation>();
+      EditorGraphAnimations[name] = std::make_shared<Engine::Animation>();
+
+      Animations[name]->SetSpriteSheet(tex, "Anim" + name + "sheet");
+      EditorGraphAnimations[name]->SetSpriteSheet(tex, "AssetGraph" + name + "sheet");
+      Animations[name]->SetFrameTime(time);
+      EditorGraphAnimations[name]->SetFrameTime(time);
+
+      std::vector<sf::IntRect> Frames = {};
+      auto frames = anim["Frames"];
+      sf::IntRect framerect = {};
+
+      for (auto & frame : frames) {
+        framerect.left = frame[0].asInt();
+        framerect.top = frame[1].asInt();
+        framerect.width = frame[2].asInt();
+        framerect.height = frame[3].asInt();
+
+        Animations[name]->AddFrame(framerect);
+        EditorGraphAnimations[name]->AddFrame(framerect);
+      }
+
+      Animations[name]->MakePingPong(bPingPong);
+      EditorGraphAnimations[name]->MakePingPong(bPingPong);
+      Animations[name]->MakeLooped(looping);
+      EditorGraphAnimations[name]->MakeLooped(looping);
+    }
   }
 
   void BasicLevel::UpdateObjectPhysics()
   {
+    static std::vector<PhysicsEngineBaseMeshType *> MeshVector;
+    static std::vector<PhysicsEngineSegmentType *> SegVector;
+
+    static std::vector<std::shared_ptr<Engine::Collider2D>> Colliders;
+
+
+    MeshVector.clear();
+    SegVector.clear();
+    Colliders.clear();
+
+    for (auto & obj : LevelObjects) {
+      auto vec = obj.second->GetColliders();
+      for (auto & mesh : vec) {
+        Colliders.push_back(mesh);
+      }
+    }
+
+    for (auto & seg : Segments)
+      SegVector.push_back(seg.get());
+
+    UpdatePhysics(Colliders, SegVector);
   }
 
 }
